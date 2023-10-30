cbf0e300e950d41283826bd140af2d4851de6381
niabezara <niabezarashvili@yahoo.com>
2023-10-30 19:17:39 +0400

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,34 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import Switch from "react-switch";
 
 export default function Header() {
+  const [checked, setCheck] = useState(false);
+
+  const handleChange = () => {
+    setCheck(!checked);
+  };
+
   return (
     <div className="Header">
       <img src="/assets/desktop/logo.svg" alt="" />
+      <label className="flex justify-between space-x-2 items-center">
+        <img src="/assets/desktop/icon-sun.svg" alt="" />
+        <Switch
+          onChange={handleChange}
+          checked={checked}
+          onColor="#fff"
+          offColor="#fff"
+          offHandleColor="#5964e0"
+          onHandleColor="#5964e0"
+          checkedIcon={false}
+          uncheckedIcon={false}
+          height={24}
+          width={48}
+          handleDiameter={16}
+        />
+        <img src="/assets/desktop/icon-moon.svg" alt="" />
+      </label>
     </div>
   );
 }
