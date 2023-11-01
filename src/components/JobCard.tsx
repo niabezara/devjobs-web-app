e66e935155e7ffb6131474ac2a6618d7f2aae98d
niabezara <niabezarashvili@yahoo.com>
2023-11-01 17:55:54 +0400

    card tablet

--- src/components/JobCard.tsx
+++ src/components/JobCard.tsx
@@ -14,31 +14,33 @@ export default function JobCard() {
   };
 
   return (
-    <div className="w-full items-center flex flex-col justify-center">
-      {visible.map((job: any) => (
-        <article
-          key={job.id}
-          className="bg-white dark:bg-[#19202D]  flex flex-col mt-9 p-8 rounded-md gap-3.5 relative w-full"
-        >
-          <div
-            className="h-[50px] w-[50px] flex items-center justify-center rounded-[15px] absolute top-[-23px]"
-            style={{ backgroundColor: job.logoBackground }}
+    <div className="w-full">
+      <div className="w-full items-center flex flex-col justify-center md:grid md:grid-cols-2 gap-4">
+        {visible.map((job: any) => (
+          <article
+            key={job.id}
+            className="bg-white dark:bg-[#19202D] h-[calc(100%-3.5rem)] flex flex-col mt-9 p-11 rounded-md gap-3.5 relative w-full"
           >
-            <img src={job.logo} alt="" />
-          </div>
-          <div className="flex items-center mt-3">
-            <p className="text-sm after:content-['.'] after:p-2 after:w-2 after:h-2 after:text-center text-[#6e8098]">
-              {job.postedAt}
-            </p>
-            <p className="text-[#6e8098] text-sm">{job.contract}</p>
-          </div>
-          <h1 className="font-bold text-base dark:text-[#fff]">
-            {job.position}
-          </h1>
-          <h4 className="text-[#6e8098] text-sm">{job.company}</h4>
-          <p className="text-[#5964e0] font-bold text-sm">{job.location}</p>
-        </article>
-      ))}
+            <div
+              className="h-[50px] w-[50px] flex items-center justify-center rounded-[15px] absolute top-[-23px]"
+              style={{ backgroundColor: job.logoBackground }}
+            >
+              <img src={job.logo} alt="" />
+            </div>
+            <div className="flex items-center mt-3">
+              <p className="text-sm after:content-['.'] after:p-2 after:w-2 after:h-2 after:text-center text-[#6e8098]">
+                {job.postedAt}
+              </p>
+              <p className="text-[#6e8098] text-sm">{job.contract}</p>
+            </div>
+            <h1 className="font-bold text-base dark:text-[#fff]">
+              {job.position}
+            </h1>
+            <h4 className="text-[#6e8098] text-sm">{job.company}</h4>
+            <p className="text-[#5964e0] font-bold text-sm">{job.location}</p>
+          </article>
+        ))}
+      </div>
       <button
         onClick={handler}
         className="bg-[#5964e0] text-white px-6 py-3 rounded-lg mt-8 mb-10 font-bold"
